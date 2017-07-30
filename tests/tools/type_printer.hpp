14b56aaa779c543163a52118ce6839999df28415
Bastien Penavayre <bastienPenava@gmail.com>
2017-07-30 18:09:28 +0200

    improve type_printer.hpp

--- tests/tools/type_printer.hpp
+++ tests/tools/type_printer.hpp
@@ -20,7 +20,6 @@
  * THE SOFTWARE.
  */
 #pragma once
-
 #include <cxxabi.h>
 #include <iostream>
 #include <type_traits>
@@ -30,101 +29,106 @@
 
 using namespace std::literals;
 
-class NoPrint {};
-
-inline std::string find_class_name(std::string const &name)
+namespace type_repr
 {
-    std::size_t b = name.size();
-    std::size_t open = 0;
+    class NoPrint {};
 
-    for (std::size_t i = 0; i < name.size(); i++)
-        if (name[i] == '>')
-            --open;
-        else if (name[i] == '<')
-            if (open++ == 0)
-                b = i;
-    return name.substr(0, b);
-}
-
-inline bool char_to_string(char c, std::string &res)
-{
-    if ((c >= 1 && c <= 6) || (c >= 14 && c <= 31) || c == 127)
-        return false;
-    if (c >= ' ')
-        res = "'"s + c + "'";
-    else
+    inline std::string find_class_name(std::string const &name)
     {
-        res = "'\\"s + std::map<char, char>({
-                {'\0', '0'}, {'\a', 'a'}, {'\b', 'b'}, {'\t', 't'}, {'\n', 'n'}, {'\v', 'v'}, {'\f', 'f'}, {'\r', 'r'}
-            })[c] + '\'';
+        std::size_t b = name.size();
+        std::size_t open = 0;
+
+        for (std::size_t i = 0; i < name.size(); i++)
+            if (name[i] == '>')
+                --open;
+            else if (name[i] == '<')
+                if (open++ == 0)
+                    b = i;
+        return name.substr(0, b);
     }
-    return true;
-}
 
-inline void charcast_replace(std::string &str)
-{
-    size_t index = 0;
-    const std::string motif = "(char)";
-    while ((index = str.find(motif, index)) != std::string::npos)
+    inline bool char_to_string(char c, std::string &res)
     {
-        size_t end = 0;
-        std::string with;
-        if (char_to_string((char)std::stoi(&str[index + motif.size()], &end), with)) {
-            str.replace(index, motif.size() + end, with);
-            index -= ((motif.size() + end) - with.size());
-        }
+        if ((c >= 1 && c <= 6) || (c >= 14 && c <= 31) || c == 127)
+            return false;
+        if (c >= ' ')
+            res = "'"s + c + "'";
+        else
+            res = "'\\"s + std::map<char, char>({
+                    {'\0', '0'}, {'\a', 'a'}, {'\b', 'b'}, {'\t', 't'},
+                    {'\n', 'n'}, {'\v', 'v'}, {'\f', 'f'}, {'\r', 'r'}
+                })[c] + '\'';
+        return true;
     }
-}
 
-template <class T = NoPrint, bool new_line = true>
-struct TypePrinter {};
-
-template <class T, bool new_line>
-std::ostream& operator<<(std::ostream &stream, TypePrinter<T, new_line>)
-{
-    if constexpr (!std::is_same_v<T, NoPrint>)
+    inline void charcast_replace(std::string &str)
     {
-        int status = -4;
-        const char *name = typeid(T).name();
-        std::string true_name = abi::__cxa_demangle(name, 0, 0, &status);
-        std::string class_name = find_class_name(true_name);
-        std::string namespe = [&]() -> std::string {
-            std::size_t i = class_name.rfind("::");
-            if (i != std::string::npos)
-                return class_name.substr(0, i);
-            else
-                return "";
-        }();
-        if (!namespe.empty()) {
-            std::string::size_type n = 0;
-            while ( ( n = true_name.find( namespe, n ) ) != std::string::npos )
-            {
-                true_name.replace( n, namespe.size(), "" );
-                n += 0;
+        size_t index = 0;
+        const std::string motif = "(char)";
+        while ((index = str.find(motif, index)) != std::string::npos)
+        {
+            size_t end = 0;
+            std::string with;
+            if (char_to_string((char)std::stoi(&str[index + motif.size()], &end), with)) {
+                str.replace(index, motif.size() + end, with);
+                index -= ((motif.size() + end) - with.size());
             }
         }
-        charcast_replace(true_name);
-        stream <<  true_name;
-        if (!namespe.empty())
-            stream << " (:: ==> " + namespe + ")";
     }
-    if constexpr (new_line) stream << std::endl;
-    return stream;
+
+    template <class T = NoPrint, bool new_line = true>
+    struct TypePrinter {};
+
+    template <class T, bool new_line>
+    std::ostream& operator<<(std::ostream &stream, TypePrinter<T, new_line>)
+    {
+        if constexpr (!std::is_same_v<T, NoPrint>)
+        {
+            int status = -4;
+            const char *name = typeid(T).name();
+            std::string true_name = abi::__cxa_demangle(name, 0, 0, &status);
+            std::string class_name = find_class_name(true_name);
+            std::string namespe = [&]() -> std::string {
+                std::size_t i = class_name.rfind("::");
+                if (i != std::string::npos)
+                    return class_name.substr(0, i);
+                else
+                    return "";
+            }();
+            if (!namespe.empty()) {
+                std::string::size_type n = 0;
+                while ( ( n = true_name.find( namespe, n ) ) != std::string::npos )
+                {
+                    true_name.replace( n, namespe.size(), "" );
+                    n += 0;
+                }
+            }
+            charcast_replace(true_name);
+            stream <<  true_name;
+            if (!namespe.empty())
+                stream << " (:: ==> " + namespe + ")";
+        }
+        if constexpr (new_line) stream << std::endl;
+        return stream;
+    }
+
+    template <class T = NoPrint, bool new_line = true>
+    void type_printer()
+    {
+        std::cout << TypePrinter<T, new_line>();
+    }
+
+    template <class T, bool new_line = true>
+    void make_type_printer(T const &) {
+        type_printer<T, new_line>();
+    }
+
+    template <class T, bool new_line = false>
+    auto make_type_printer()
+    {
+        return TypePrinter<T, new_line>();
+    }
 }
 
-template <class T = NoPrint, bool new_line = true>
-void type_printer()
-{
-    std::cout << TypePrinter<T, new_line>();
-}
-
-template <class T, bool new_line = true>
-void make_type_printer(T const &) {
-    type_printer<T, new_line>();
-}
-
-template <class T, bool new_line = false>
-auto make_type_printer()
-{
-    return TypePrinter<T, new_line>();
-}
+using type_repr::type_printer;
+using type_repr::make_type_printer;
